1902447a602f058bae8f6e0f81285ea9988d838d
PaxPrz <39772445+PaxPrz@users.noreply.github.com>
2018-07-15 17:40:06 +0545

    Basic UI creation in game
    Score
    Stress Meter
    Pause Button
    (No content inside pause button)

---
+++ Assets/Scripts/PlaySettings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaySettings : MonoBehaviour {
+	[SerializeField]
+	public static bool paused;
+	public Canvas pauseCanvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onPause(){
+		paused = true;
+		Time.timeScale = 0f;
+		pauseCanvas.gameObject.SetActive (true);
+	}
+}
